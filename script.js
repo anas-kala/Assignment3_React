09ee57afe659f270873aa55f0da34ac76d1ac910
Anas Al Kala <anas.kala@hotmail.com>
2021-03-30 17:22:41 +0200

    new changes

--- script.js
+++ script.js
@@ -28,7 +28,41 @@ form.addEventListener('submit', function (event) {
     var formData = new FormData(form);
     console.log('Form Submitted');
     console.log("first name: " + formData.get('subject') + "\nlastName: " + formData.get('body'));
+    var msgSubject = formData.get('subject');
+    var msgBody = formData.get('body');
+    var newMsg = new Message(msgSubject, msgBody);
+    msgs.AddToList(newMsg);
+    document.getElementById('form').style.display = 'none';
+    showList();
+    document.getElementById('allMessages').style.display = "block";
     event.preventDefault();
     form.reset();
     document.getElementById('form').reset();
 });
+function showList() {
+    var subjectNode = document.createElement("h2");
+    subjectNode.style.color = "red";
+    var subjectText = document.createTextNode(msgs.messages[msgs.messages.length - 1].subject);
+    subjectNode.appendChild(subjectText);
+    var bodyNode = document.createElement("p");
+    var bodyText = document.createTextNode(msgs.messages[msgs.messages.length - 1].body);
+    bodyNode.appendChild(bodyText);
+    var divContainer = document.createElement('div');
+    divContainer.appendChild(subjectNode);
+    divContainer.appendChild(bodyText);
+    var lineNode = document.createElement('hr');
+    divContainer.appendChild(lineNode);
+    document.getElementById('allMessages').appendChild(divContainer);
+    if (msgs.messages[msgs.messages.length - 1].read == false) {
+        divContainer.style.backgroundColor = 'green';
+    }
+}
+function showMessages() {
+    document.getElementById('allMessages').style.display = 'block';
+    document.getElementById('form').style.display = 'none';
+    showList();
+}
+function addNewMessage() {
+    document.getElementById('allMessages').style.display = 'none';
+    document.getElementById('form').style.display = 'block';
+}
--- script.ts
+++ script.ts
@@ -27,15 +27,53 @@ class Messages {
 
 const form = document.getElementById("form") as HTMLFormElement;
 const button: HTMLElement = document.getElementById("button") as HTMLButtonElement;
-const msgs=new Messages();
+const msgs = new Messages();
 
 
 form.addEventListener('submit', function (event) {
     const formData = new FormData(form);
     console.log('Form Submitted');
     console.log(`first name: ${formData.get('subject')}\nlastName: ${formData.get('body')}`);
+    var msgSubject = formData.get('subject') as string;
+    var msgBody = formData.get('body') as string;
+    var newMsg = new Message(msgSubject, msgBody);
+    msgs.AddToList(newMsg);
+    (document.getElementById('form') as HTMLFormElement).style.display = 'none';
+    showList();
+    (document.getElementById('allMessages') as HTMLFormElement).style.display = "block";
     event.preventDefault();
     form.reset();
     (document.getElementById('form') as HTMLFormElement).reset()
 });
+
+function showList() {
+    var subjectNode = document.createElement("h2");
+    subjectNode.style.color = "red";
+    var subjectText = document.createTextNode(msgs.messages[msgs.messages.length - 1].subject);
+    subjectNode.appendChild(subjectText);
+    var bodyNode = document.createElement("p");
+    var bodyText = document.createTextNode(msgs.messages[msgs.messages.length - 1].body);
+    bodyNode.appendChild(bodyText);
+    var divContainer=document.createElement('div');
+    divContainer.appendChild(subjectNode);
+    divContainer.appendChild(bodyText);
+    var lineNode = document.createElement('hr');
+    divContainer.appendChild(lineNode);
+    document.getElementById('allMessages').appendChild(divContainer);
+    if(msgs.messages[msgs.messages.length-1].read==false){
+        divContainer.style.backgroundColor='green';
+    }
+
+}
+
+function showMessages() {
+    document.getElementById('allMessages').style.display = 'block';
+    document.getElementById('form').style.display = 'none';
+    showList();
+}
+
+function addNewMessage() {
+    document.getElementById('allMessages').style.display = 'none';
+    document.getElementById('form').style.display = 'block';
+}
 
